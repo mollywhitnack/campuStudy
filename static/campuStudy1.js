3a4ce4c96a5bee8209a5a21a73ace5eab1065035
Molly Whitnack <mwhitnac@ucsd.edu>
2015-02-07 23:15:33 +0000

    Cahnges pixels to percents

--- static/campuStudy1.js
+++ static/campuStudy1.js
@@ -6,9 +6,30 @@ $(document).ready(function() {
 })
 
 
+$(".nav-tabs").on("click", "a", function(e){
+      e.preventDefault();
+      $(this).tab('show');
+    })
+    .on("click", "span", function () {
+        var anchor = $(this).siblings('a');
+        $(anchor.attr('href')).remove();
+        $(this).parent().remove();
+        $(".nav-tabs li").children('a').first().click();
+    });
+
+    $('.add-contact').click(function(e) {
+        e.preventDefault();
+        var id = $(".nav-tabs").children().length; //think about it ;)
+        $(this).closest('li').before('<li><a href="#contact_'+id+'">New Tab</a><span>x</span></li>');         
+        $('.tab-content').append('<div class="tab-pane" id="contact_'+id+'">Contact Form: New Contact '+id
+
+
+
+
+
 //<strong>$(this).text</strong><strong>(</strong>anagrammedName($(this).text())<strong>)</strong>;
 
-function initializePage() 
+/*function initializePage() 
 {
   console.log("Javascript connected!");
   $("#dropDown").click(listenerfunction);
@@ -19,7 +40,7 @@ function listenerfunction(e)
 {
   e.preventDefault();
 }
-
+*/
 
       // Grab the tab links and content divs from the page
      /* var tabListItems = document.getElementById('tabs').childNodes;
